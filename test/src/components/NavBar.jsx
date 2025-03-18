43e7960f82c44fd31c3cc4b77f1a3e9c00fcf093
wassila <rzgwassila@gmail.com>
2025-03-18 15:44:03 +0100

    update navbar

--- test/src/components/NavBar.jsx
+++ test/src/components/NavBar.jsx
@@ -8,11 +8,12 @@ function Navbar() {
         <li>
           <a href="#home">Home</a>
         </li>
-        <li>
-          <a href="#agenda">Agenda</a>
-        </li>
+
         <li>
           <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#agenda">Agenda</a>
         </li>
         <li>
           <a href="#faq">FAQ</a>
